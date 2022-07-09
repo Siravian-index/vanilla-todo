4a802b432dc6e8d8d8f3043360faa34beee32db8
David <depch47@gmail.com>
2022-07-09 11:28:36 -0500

    DOM manipulation through javascipt

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,46 @@
 "use strict";
-const main = () => {
+const initForm = () => {
     const root = document.querySelector('#root');
-    const ul = document.createElement('ul');
-    const names = ['david', 'santiago', 'maria'];
-    const lis = names.map((name, index) => {
-        const li = document.createElement('li');
-        li.innerText = `Hello ${name}`;
-        li.id = `${index}-element`;
-        return li;
+    const form = document.createElement('form');
+    const title = document.createElement('input');
+    const body = document.createElement('textarea');
+    const submitBtn = document.createElement('button');
+    title.placeholder = 'To-do Name';
+    title.id = 'title';
+    body.placeholder = 'Content';
+    body.id = 'body';
+    submitBtn.innerText = 'Submit';
+    form.append(title, body, submitBtn);
+    root.append(form);
+    return form;
+};
+const printToScreen = (todo) => {
+    const ul = document.querySelector('#list');
+    if (!ul) {
+        const newUl = document.createElement('ul');
+        const root = document.querySelector('#root');
+        newUl.id = 'list';
+        root.append(newUl);
+    }
+};
+const handleSubmit = (form) => {
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const title = document.querySelector('#title');
+        const body = document.querySelector('#body');
+        if (title && body) {
+            const newTodo = { title: title.value, body: body.value };
+            console.log(newTodo);
+            // fetch
+            // printToScreen
+            // clean fields
+        }
+        // const [title, content] = form.children
+        // console.log(title, content)
     });
-    ul.append(...lis);
-    root.append(ul);
-    console.log(root);
+};
+const main = () => {
+    const form = initForm();
+    handleSubmit(form);
 };
 main();
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,16 +1,58 @@
-const main = () => {
+interface todo {
+  title: string
+  body: string
+}
+
+const initForm = () => {
   const root = document.querySelector('#root') as HTMLDivElement
-  const ul = document.createElement('ul')
-  const names = ['david', 'santiago', 'maria']
-  const lis = names.map((name, index) => {
-    const li = document.createElement('li')
-    li.innerText = `Hello ${name}`
-    li.id = `${index}-element`
-    return li
+  const form = document.createElement('form')
+  const title = document.createElement('input')
+  const body = document.createElement('textarea')
+  const submitBtn = document.createElement('button')
+
+  title.placeholder = 'To-do Name'
+  title.id = 'title'
+  body.placeholder = 'Content'
+  body.id = 'body'
+  submitBtn.innerText = 'Submit'
+
+  form.append(title, body, submitBtn)
+  root.append(form)
+  return form
+}
+
+const printToScreen = (todo: todo) => {
+  const ul = document.querySelector('#list') as HTMLUListElement | null
+  if (!ul) {
+    const newUl = document.createElement('ul')
+    const root = document.querySelector('#root') as HTMLDivElement
+    newUl.id = 'list'
+    root.append(newUl)
+  }
+}
+
+const handleSubmit = (form: HTMLFormElement) => {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const title: HTMLInputElement | null = document.querySelector('#title')
+    const body: HTMLInputElement | null = document.querySelector('#body')
+    if (title && body) {
+      const newTodo = { title: title.value, body: body.value }
+      console.log(newTodo)
+
+      // fetch
+      // printToScreen
+      // clean fields
+    }
+
+    // const [title, content] = form.children
+    // console.log(title, content)
   })
-  ul.append(...lis)
-  root.append(ul)
-  console.log(root)
+}
+
+const main = () => {
+  const form = initForm()
+  handleSubmit(form)
 }
 
 main()
